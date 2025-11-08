e13fe7f351dd33fa8512b086726bdb9d849b1391
EugeniuEnachii <112726772+EugeniuEnachii@users.noreply.github.com>
2025-11-08 15:57:32 -0500

    Assign-VideoID v1
    From a CSV, takes the video links , removes the duplicates, then gives them a unique identifier.

--- Assign-VideoID.ps1
+++ Assign-VideoID.ps1
@@ -5,17 +5,56 @@ param(
 $MainCSV = Import-Csv -Path $PathCSV -Delimiter ';'
 $VideoLinkSource = Import-Csv -Path .\UniqueVideoSources_backup.csv -Delimiter ';' #PathToTheDelimeters
 
-$UniqueVideoLinks = @() #Array of VideoID - Link hashtables
+$UniqueVideoLinks = @()        # Array of VideoID - Link hashtables
 
-$Incremental = 0 #Increment for VideoID
-$FormattedIncremental = "{0:D4}" -f $Incremental #Padding for '0000' formatting
+$Incremental = 0                # Increment for VideoID
+$FormattedIncremental           # Padding for '0000' formatting
 
 
-foreach($line in $MainCSV){ #For each line in the Main CSV...
+foreach($line in $MainCSV){     # For each line in the Main CSV...
 
-    #$VideoID #The VideoID
-    $Diminutive # Diminutive of the VideoID
+    $VideoID               = "" # The VideoID (ex : 0001-YOUTUBE)
+    $Diminutive            = "" # Diminutive of the VideoID (ex : YOUTUBE)
+    $Domain                = "" # Link related to the diminutive (ex : www.youtube.com)
+    $AppendableHashtable   = "" # It's a hashtable that will get appended to $UniqueVideoLinks (ex : @{VideoID = "0001-YOUTUBE" ; Link = "www.youtube.com/watch?v=akjsKdSKJakah" })
 
+
+# =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^= a  =^..^=   =^..^=   =^..^=    =^..^=    =^..^=    =^..^=   
     
 
+    if($line.LinkFROriginal -match '^(?:https?:\/\/)?([^\/]+)') {
+
+        if($line.LinkFROriginal -notin ($UniqueVideoLinks.Link)){
+            $Domain = $matches[1] #Gives a source link (www.youtube.com)
+            $entry = $VideoLinkSource | Where-Object { $_.Domain -eq $domain } # Hashtable (@{Domain=milwaukeetool.widen.net; Diminutive=WIDENNET})
+            $Diminutive = $entry.Diminutive
+            
+            $Incremental++ #Increment for VideoID
+            $FormattedIncremental = "{0:D4}" -f $Incremental #Updates the incremental formatting
+
+            $VideoID = "$FormattedIncremental-$Diminutive"
+            
+            $AppendableHashtable = [PSCustomObject]@{VideoID = "$VideoID" ; Link = "$($line.LinkFROriginal)"} 
+            $UniqueVideoLinks += $AppendableHashtable
+        }    
+    }
+
+    if($line.LinkENOriginal -match '^(?:https?:\/\/)?([^\/]+)') {
+
+        if($line.LinkENOriginal -notin ($UniqueVideoLinks.Link)){
+            $Domain = $matches[1] #Gives a source link (www.youtube.com)
+            $entry = $VideoLinkSource | Where-Object { $_.Domain -eq $domain } # Hashtable (@{Domain=milwaukeetool.widen.net; Diminutive=WIDENNET})
+            $Diminutive = $entry.Diminutive
+            
+            $Incremental++ #Increment for VideoID
+            $FormattedIncremental = "{0:D4}" -f $Incremental #Updates the incremental formatting
+
+            $VideoID = "$FormattedIncremental-$Diminutive"
+            
+            $AppendableHashtable = [PSCustomObject]@{VideoID = "$VideoID" ; Link = "$($line.LinkENOriginal)"} 
+            $UniqueVideoLinks += $AppendableHashtable
+        }    
+    }
+    
 }
+$UniqueVideoLinks
